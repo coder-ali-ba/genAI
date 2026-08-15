bc59289275ceac7494d159d537cb16e998f8c611
coder-ali-ba <habibali8770@gmail.com>
2026-08-15 19:37:58 +0500

    cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 
-import express from "express";
+import express, { urlencoded } from "express";
 import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors"
@@ -9,6 +9,7 @@ const app =express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
+app.use(express.urlencoded({extended:true}))
 
 app.use("/api/auth" , authRouter)
 app.use("/api/interview" , interviewRouter)
