b1b5f904b2676a22b50f4762a8336eaaf24d0f2a
coder-ali-ba <habibali8770@gmail.com>
2026-08-15 19:33:49 +0500

    cors solved

--- src/app.js
+++ src/app.js
@@ -8,10 +8,7 @@ import interviewRouter from "./routes/interview.routes.js";
 const app =express()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin : "http://localhost:5173",
-    credentials : true
-}))
+app.use(cors())
 
 app.use("/api/auth" , authRouter)
 app.use("/api/interview" , interviewRouter)
