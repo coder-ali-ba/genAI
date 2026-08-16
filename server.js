10fbeb6cd4a06877624ebdb7e8128e0a71a80071
coder-ali-ba <habibali8770@gmail.com>
2026-08-16 18:51:47 +0500

    server.js fix

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
-import  dotenv  from "dotenv";
-dotenv.config()
-// import configEnv from "./src/config/dotenv.js"
-// configEnv()
+// import  dotenv  from "dotenv";
+// dotenv.config()
+// // import configEnv from "./src/config/dotenv.js"
+// // configEnv()
 
-import app from "./src/app.js"
+// import app from "./src/app.js"
 
 
-import connectToDB from "./src/config/database.js"
-connectToDB()
+// import connectToDB from "./src/config/database.js"
+// connectToDB()
 
+
+//     app.listen(3000, () => {
+//         console.log("server running on http://localhost:3000");
+//     });
+
+
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./src/app.js";
+import connectToDB from "./src/config/database.js";
+
+const startServer = async () => {
+  try {
+    await connectToDB();
 
     app.listen(3000, () => {
-        console.log("server running on http://localhost:3000");
+      console.log("server running on http://localhost:3000");
     });
+  } catch (error) {
+    console.error("Database connection failed:", error);
+    process.exit(1);
+  }
+};
 
-
+startServer();
